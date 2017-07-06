52cdc5ccca1179f4cbdf19eab6725386ddaf3092
dantefarias <fariasdante.94@gmail.com>
2017-07-06 00:04:45 -0300

    head

---
+++ String/Practica9.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+pair<int,int> meseta (vector<int> vec);
+vector<vector<int> > ejer2 (vector<vector<int>> base);
+vector <pair<int,int> > ejer4 (vector<int> vec1, vector<int> vec2);
+int ejer5 (vector<int> vec1, vector<int> vec2);
+int ejer6 (vector<int> vec1, int k);
+int ejer7 (vector<int> vec);
+vector<pair<int,int> > ejer8 (vector<int> vec);
+bool subSeq (string cad1, string cad2);
+string ejer10 (string cad1, string cad2);
+vector<pair<int,int> > subsec (string cad1, string cad2);
+string armarsub (string cad1, int i, int f);
+vector<string> sacaDelim (string cadena, char delim);
+string InvertirFrase (string cadena, char delimitador);
+vector<string> split (string cadena);
+vector<int> esMeseta (vector<int> vec);
